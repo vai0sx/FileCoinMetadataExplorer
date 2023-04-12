06d5bb424fbbb915b4094eb5caca7c5a6297027e
vai0sx <130502134+vai0sx@users.noreply.github.com>
2023-04-12 14:50:58 -0600

    Create db.js

---
+++ db.js
@@ -0,0 +1,6 @@
+const { MongoClient } = require('mongodb');
+
+const uri = 'mongodb://localhost:27017';
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+module.exports = client;
